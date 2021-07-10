5b7524bbfbee97db6599b5bf31eddc60027c6517
Daria Romanova <romanovadi@gmail.com>
2021-07-10 23:03:08 +0300

    Cases were optimized, calculation of loss function fixed
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- constantAngleSlopeTurbKWUProfileInlet100621GLZ/runStep.sh
+++ constantAngleSlopeTurbKWUProfileInlet100621GLZ/runStep.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 . ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions
-pyFoamPrepareCase.py --no-complain --quiet --no-write-parameters --no-write-report . >log.pyFoam
+./clearStep.sh
+pyFoamPrepareCase.py --no-clear --stop-after-templates --no-complain --quiet --no-write-parameters --no-write-report . 2>&1 > log.pyFoam
 
-runApplication setFields
-runApplication decomposePar -constant
 runParallel $(getApplication)
-runParallel postProcess -parallel -func singleGraph
+runParallel postProcess -parallel -latestTime -func singleGraph
 python3 calcLoss.py
-
-#pisoFoam > solver.log
-#postProcess -field p -latestTime
--- constantAngleSlopeTurbKWUProfileInlet230421GLZ/calcLoss.py
+++ constantAngleSlopeTurbKWUProfileInlet230421GLZ/calcLoss.py
@@ -6,32 +6,41 @@ import math
 from functools import partial
 
 def readHV(fileName):
-	df = pd.read_csv(fileName, sep='\t', header=None)
-	HV = df.to_numpy()
-	HV = normalizeHV(HV)
+	try:
+		df = pd.read_csv(fileName, sep='\t', header=None)
+		HV = df.to_numpy()
+		HV[:, 0] = HV[:, 0] * 0.001
+		HV = normalizeHV(HV)
+	except:
+		H = np.arange(0, 10, 0.1, dtype=float)
+		V = np.full(100, 100)
+		HV = np.column_stack((H, V))
 	return HV
 
 def readHVPostProc(fileName):
-	df = pd.read_csv(fileName, sep='\t', header=None)
-	HV = df.iloc[:, [0,1]].to_numpy()
-	HV = normalizeHV(HV)
+	try:
+		df = pd.read_csv(fileName, sep='\t', header=None)
+		HV = df.iloc[:, [0,1]].to_numpy()
+		HV = normalizeHV(HV)
+	except:
+		H = np.arange(0, 10, 0.1, dtype=float)
+		V = np.zeros(100)
+		HV = np.column_stack((H, V))
 	return HV
 
 def normalizeHV(HV):
 	Hstart = HV[0,0]
 	Hfinish = HV[-1,0]
-	Hstep = 0.0001
+	Hstep = 0.00001
 	H = np.arange(Hstart, Hfinish + Hstep, Hstep, dtype=float)
 	V = np.interp(H, HV[:,0], HV[:,1], left=0, right=0)
 	HV = np.column_stack((H, V))
 	return HV
 
 def calcOneRMSE(HVRef, HVTmp):
-	URef = waterVelocityExtract(HVRef[:,1])
-	UTmp = waterVelocityExtract(HVTmp[:,1])
-	size = min(URef.size, UTmp.size)
-	URef = URef[:size]
-	UTmp = UTmp[:size]
+	size = min(waterVelocityExtract(HVRef[:,1]), waterVelocityExtract(HVTmp[:,1]))
+	URef = HVRef[:size, 1]
+	UTmp = HVTmp[:size, 1]
 	RMSE = (URef - UTmp)**2
 	RMSE =  math.sqrt(RMSE.sum() / size)
 	return RMSE
@@ -40,13 +49,12 @@ def waterVelocityExtract(U):
 	nMax = np.amax(np.argmax(U))
 	if nMax != U.size - 1:
 		nMax += 1
-	return U[:nMax]
+	return nMax
 
 def main():
-	bounds = [[0.5, 1.0], [0.5, 2.5], [0.2, 0.8], [0.7, 1.0], [0.0, 0.15], [0.0, 0.15], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.5, 1.5], [5.0, 15.0]]
+	bounds = [[0., 2.], [0., 2.], [0., 2.], [0., 2.], [0., 0.5], [0., 0.5], [0., 0.5], [0., 2.], [0., 2.], [0., 2.], [0., 2.], [0., 20.]]
 	coeffs = [0.85, 1.0, 0.5, 0.856, 0.075, 0.0828, 0.09, 0.5555556, 0.44, 0.31, 1.0, 10.0]
-#	coeffs = [0.09]
-	refFile = ("data/100621OutletSmoothProfile.csv")
+	refFile = ("data/outletExperimentalProfile.csv")
 	turbFile = ("postProcessing/singleGraph/0.5/line_U.xy")
 	dfHVRef = readHV(refFile)
 	dfHVPostProc = readHVPostProc(turbFile)
